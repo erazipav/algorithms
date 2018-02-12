c22d40a60f5590cd308ef0c499aaafae5236698d
Pavel Erazim <pavel.erazim@cloudevelops.com>
2018-02-12 22:07:28 +0100

    Renamed AG1 to graphs

---
+++ graphs/jarnik.cpp
@@ -0,0 +1,14 @@
+#include <cstdlib>
+#include <iostream>
+
+// HERE WILL BE MY JARNIK(PRIM'S) ALGORITHM IMPLEMENTATION
+
+// notes: use heap for newly added available edges
+// will need - output edges that form skeleton,currently available edges, all available edges
+
+using namespace std;
+
+int main() {
+
+
+}
